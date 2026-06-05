952ac5c272e0931c7d37b2153684b7bcbf87016f
varad ghare <gharev653@gmail.com>
2026-06-05 12:47:44 +0530

    successfully changed

--- finding an largest and largest element from the array.java
+++ finding an largest and largest element from the array.java
@@ -1,1 +1,28 @@
-helloworld
+class demo{
+    public static void main(String args[]){
+
+         // finding the largest and the second largest element from an array...
+
+        int arr[] =  {12,24,34,32,45,23};
+        int secondLargest = Integer.MIN_VALUE;
+        int largest = Integer.MIN_VALUE;
+
+        for(int i = 0;i<=arr.length-1;i++){
+
+            if(arr[i] > largest){
+                secondLargest = largest;
+                largest = arr[i];
+            }
+            else if(arr[i] > secondLargest && secondLargest != largest){
+                secondLargest = arr[i];
+            }
+
+
+        }
+        System.out.println("secondlargest element us : " + secondLargest);
+        System.out.println("largest element us : " + largest);
+
+
+
+    }
+}
