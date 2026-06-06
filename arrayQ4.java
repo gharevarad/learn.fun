73986333b22a112f1ba82c4cfd14027f4c65336e
varad ghare <gharev653@gmail.com>
2026-06-07 02:46:08 +0530

    program for getting dublicatevalues from array

--- arrayQ4.java
+++ arrayQ4.java
@@ -1,1 +1,31 @@
+class arrayQ4{
+    public static void main(String args[]){
 
+        // get a dublicate element from an array
+
+        int arr[] = {12,2,3,34,4,7};
+        boolean dublicateValue = false;
+        int i;
+
+        for( i = 0;i < arr.length;i++){
+
+            for(int j = i+1;j < arr.length;j++){
+
+                if((arr[i] ^ arr[j]) == 0){
+                    System.out.println(arr[i] + " the number is dublicate");
+                    dublicateValue = true;
+                    break;
+                }
+                
+                
+            }
+
+            
+
+        }
+        if(!dublicateValue){ 
+            System.out.println("array doesnt have a dublicate value...");
+        }
+
+    }
+}
