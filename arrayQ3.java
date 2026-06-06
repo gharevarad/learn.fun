dc14cb344d6ec338afb380e7dc2dc4b7b2b0c33d
varad ghare <gharev653@gmail.com>
2026-06-07 00:52:02 +0530

    code for checking array is sorted or not

--- arrayQ3.java
+++ arrayQ3.java
@@ -1,1 +1,28 @@
+class arrayQ3{
+    public static void main(String args[]){
 
+        // checking the array is sorted or not 
+
+        int arr[] = {1,2,23,4,5};
+        boolean isSorted = true;
+
+        for(int i = 1 ; i < arr.length ; i++ ){
+
+            if(arr[i-1] > arr[i]){
+                isSorted = false;
+                break;
+            }
+
+            
+        }
+        if(isSorted){
+                System.out.println("array is in the sorted order...");
+            }
+            else{
+                System.out.println("array is not in the sorted order...");
+            }
+
+
+    }
+
+}
