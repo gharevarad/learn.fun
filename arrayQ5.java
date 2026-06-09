db80843f79eb4e8fff98f9871ee92b6162266cc5
varad ghare <gharev653@gmail.com>
2026-06-09 15:50:43 +0530

    finding an missing value from an array

--- arrayQ5.java
+++ arrayQ5.java
@@ -1,1 +1,27 @@
+class arrayQ5{
+    public static void main(String args[]){
 
+        // finding an missing value from an array
+
+        int arr[] = {1,2,3,5,7,10};
+        int n = 10;
+        boolean matched = false;
+        int numbers = 0;
+        System.out.print("missing number is : ");
+        for(int num = 1 ; num <= n ; num ++){
+            matched = false;
+        
+
+            for(int i = 0 ; i < arr.length; i++){
+                if(arr[i] == num){
+                    matched = true;
+                    break;
+                }
+            }
+
+                if(!matched){
+                    System.out.print( num + " ");
+                }             
+        } 
+    }
+}
